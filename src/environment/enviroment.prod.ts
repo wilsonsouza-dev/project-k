c055401df604bcff8138e5be7fc669bcebe7d129
wilsonsouza-dev <wilsonsouza.dev@gmail.com>
2025-04-08 21:50:21 -0400

    ajust prod env

--- src/environment/enviroment.prod.ts
+++ src/environment/enviroment.prod.ts
@@ -1,6 +1,13 @@
+const scopes = ['user-read-private', 'user-read-email', 'user-top-read'].join(
+  '%20'
+); // Transforma a lista em uma string para a URL
+
 export const environment = {
   production: true,
-  apiUrl: 'https://minhaapi.com/api',
-  spotifyClientId: 'SEU_CLIENT_ID_PROD',
-  featureToggle: false,
+  apiUrl: 'https://spotify.kwaps.online/api/spotify',
+  youtubeApiUrl: 'https://youtube.kwaps.online/api/youtube',
+  spotifyAuthUrl: 'https://accounts.spotify.com/authorize',
+  spotifyClientId: '9c9ac779f0e54ee7af47d03d14f4ba6c',
+  spotifyRedirectUri: 'http://localhost:4200/spotify/callback',
+  spotifyScope: scopes,
 };
